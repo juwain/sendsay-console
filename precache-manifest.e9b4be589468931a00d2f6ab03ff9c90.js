bd37a0133b83f2cd2ff6013c3a9789a3e657c005
Zyuzin Vitaly <juwain@yandex.ru>
2020-07-02 17:18:27 +0300

    Updates

---
+++ precache-manifest.e9b4be589468931a00d2f6ab03ff9c90.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0293e2ca55cdb90009da6836ec13bcd2",
+    "url": "/sendsay-console/index.html"
+  },
+  {
+    "revision": "1414fccc392a017e3941",
+    "url": "/sendsay-console/static/css/2.fe1c1d21.chunk.css"
+  },
+  {
+    "revision": "d2a48c845e2bd3c1e113",
+    "url": "/sendsay-console/static/css/main.0647fa83.chunk.css"
+  },
+  {
+    "revision": "1414fccc392a017e3941",
+    "url": "/sendsay-console/static/js/2.f2746fff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sendsay-console/static/js/2.f2746fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a48c845e2bd3c1e113",
+    "url": "/sendsay-console/static/js/main.6aad4514.chunk.js"
+  },
+  {
+    "revision": "9fced8c04fbb142461fe",
+    "url": "/sendsay-console/static/js/runtime-main.681d57f7.js"
+  },
+  {
+    "revision": "15b27ee324fc3ad84440a6a17e56c9cb",
+    "url": "/sendsay-console/static/media/icon-cross.15b27ee3.svg"
+  },
+  {
+    "revision": "2a2f53ccf582b42d3550e15e17138f73",
+    "url": "/sendsay-console/static/media/icon-dots.2a2f53cc.svg"
+  },
+  {
+    "revision": "c03b8910fadc8be75b562e5d3fdba2b3",
+    "url": "/sendsay-console/static/media/icon-format.c03b8910.svg"
+  },
+  {
+    "revision": "7e47c8876132a57a2724db279808aa48",
+    "url": "/sendsay-console/static/media/icon-fullscreen.7e47c887.svg"
+  },
+  {
+    "revision": "fe55b663227470acbfe9d67895de4ed9",
+    "url": "/sendsay-console/static/media/icon-loading.fe55b663.svg"
+  },
+  {
+    "revision": "e4d323ce2930a0329dce3bc953e561e8",
+    "url": "/sendsay-console/static/media/icon-logout.e4d323ce.svg"
+  },
+  {
+    "revision": "01e44a25f7837fa9ae80324f43d47ae4",
+    "url": "/sendsay-console/static/media/icon-partscreen.01e44a25.svg"
+  },
+  {
+    "revision": "3fb0ed3de63bfc614a517b683f282da1",
+    "url": "/sendsay-console/static/media/icon-smile-meh.3fb0ed3d.svg"
+  },
+  {
+    "revision": "ae1288365bf80cc20c9b75a38416e89e",
+    "url": "/sendsay-console/static/media/logo.ae128836.svg"
+  }
+]);
